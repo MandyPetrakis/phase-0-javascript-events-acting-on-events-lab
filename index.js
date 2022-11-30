641880c02c71b7b9ef5380edbe1fe9f5ac7dc31d
Mandy Petrakis <mandypetrakis@gmail.com>
2022-11-30 15:22:09 -0800

    commit tree dd0fd4977ad3c7ea5961af74b30f987418eea82e

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Your code here
+const dodger = document.getElementById("dodger");
+
+document.addEventListener("keydown", (event) => {
+  if (event.key === "ArrowLeft") {
+    moveDodgerLeft();
+  }
+  if (event.key === "ArrowRight") {
+    moveDodgerRight();
+  }
+});
+
+function moveDodgerLeft() {
+  const currentLeft = dodger.style.left.replace("px", "");
+  let left = parseInt(currentLeft, 10);
+
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
+  }
+}
+
+function moveDodgerRight() {
+  const currentRight = dodger.style.left.replace("px", "");
+  let right = parseInt(currentRight, 10);
+
+  if (right < 360) {
+    dodger.style.left = `${right + 1}px`;
+  }
+}
